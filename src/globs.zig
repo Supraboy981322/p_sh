e90cca8ebdc06bd27dd2a1764dcd606de50840eb
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-09 13:46:50 -0500

    helper for separators

--- src/globs.zig
+++ src/globs.zig
@@ -7,3 +7,15 @@ pub const stdout = &@constCast(&stdout_file.writer(&stdout_buf)).interface;
 var stderr_buf:[1024]u8 = undefined;
 pub const stderr_file = std.fs.File.stderr();
 pub const stderr = &@constCast(&stderr_file.writer(&stderr_buf)).interface;
+
+pub const separators = [_]u8{
+    ' ',  //space
+    '\t', //tabs
+    '\n', //newline
+    '/',  //forward slash
+    '\'', //single quote
+    '"',  //double quote
+    ';',  //semi-colon
+    '|',  //pipe
+    '.',  //dot
+};
