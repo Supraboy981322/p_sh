988bc9db3c66523b4d3f211159d0a3f08e6150e2
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-10 11:30:52 -0500

    helper to peek or print todo

--- src/helpers.zig
+++ src/helpers.zig
@@ -1,6 +1,20 @@
+const Term = @import("term.zig").Term;
+
 pub fn contains(list:[]u8, thing:u8) bool {
     return for (list) |itm| {
         if (thing == itm) break true;
     } else
         false;
+}
+
+pub fn peek_or_todo(term:Term, buf:[]u8, i:usize, comptime done:?u8, comptime context:[]const u8) bool {
+    if (buf.len <= i+1) return false;
+    if (buf[i+1] == done)
+        return true
+    else
+        @constCast(&term).TODO(
+            "handle " ++ (if (done) |_| "|{c}| ({x}) " else "") ++ "[{s}] {{{x}}} " ++ context,
+            if (done) |_| .{ buf[i+1], buf[i+1], buf, buf } else .{ buf, buf }
+        );
+    return false;
 }
