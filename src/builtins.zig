1871d88f961702796f1f99951e8517adde3fcbec
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-10 13:40:14 -0500

    move builtins to separate file

---
+++ src/builtins.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+const Term = @import("term.zig").Term;
+const exec = @import("exec.zig");
+
+const Cmd = exec.Cmd;
+
+pub const Valid = enum {
+    exit,
+    cd,
+};
+
+pub fn do(term:*Term, name:Valid, cmd:Cmd) !void {
+    const alloc = term.alloc;
+    var argv = try std.ArrayList([]const u8).initCapacity(alloc, 0);
+    defer {
+        for (argv.items) |a| alloc.free(a);
+        _ = argv.deinit(alloc);
+    }
+
+    for (std.mem.span(cmd.split)) |arg| if (arg) |a| {
+        try argv.append(alloc, std.mem.span(a));
+    };
+
+    switch (name) {
+        .cd => try cd(term, argv.items),
+        .exit => {},// TODO: handle this
+    }
+}
+
+pub fn cd(term:*Term, argv:[][]const u8) !void {
+    if (argv.len < 2) {
+        term.print_error("not enough args; need a directory", .{});
+        return error.@"file not found";
+    }
+    try term.cd(@constCast(argv[1]));
+}
