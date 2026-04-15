afb4a01ecbf6e5caceb244654f7f5553d9ccf3f5
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-04-15 09:41:27 -0500

    refactor piping to a file ; temp file

--- src/exec.zig
+++ src/exec.zig
@@ -25,10 +25,12 @@ pub const PipeDetails = struct {
     out:bool = false,
     in:bool = false,
     file:File = .{},
+
     pub const File = struct {
         do:bool = false,
         name:[]u8 = undefined,
         append:bool = false,
+        tmp_file:?[]u8 = null,
         in_or_out:Direction = undefined,
         pub const Direction = enum {
             IN,
@@ -112,23 +114,36 @@ pub fn populate_fd_sets(term:*Term, res:*std.ArrayList(Cmd)) !bool {
 
         const pipe_details = cmd.opts.pipe_details;
         if (pipe_details.file.do) {
-            const name = pipe_details.file.name;
             const pipein = pipe_details.file.in_or_out == .IN;
-            var file =
-                if (!pipein)
-                    try term.cwd().createFile(name, .{
-                        .truncate = !pipe_details.file.append and !pipein,
-                        .read = pipein,
-                }) else b: {
-                    cmd.opts.pipe_details.in = true;
-                    break :b try term.cwd().openFile(name, .{});
-                };
+            if (pipein) cmd.opts.pipe_details.in = true;
+            const in_fd =
+                if (pipe_details.file.tmp_file) |content| b: {
+                    const fd = try std.posix.memfd_create(
+                        content, 0 //std.posix.FD_CLOEXEC
+                    );
+                    var file = std.fs.File{ .handle = fd };
+                    _ = try file.write(content);
+                    break :b file.handle;
+                } else if (pipein)
+                    (try term.cwd().openFile(pipe_details.file.name, .{})).handle
+                else
+                    term.stdin_file.handle;
+
+            const out_fd =
+                if (cmd.opts.pipe_details.file.in_or_out == .OUT)
+                    (try term.cwd().createFile(pipe_details.file.name, .{
+                        .truncate = !pipe_details.file.append,
+                        .read = true,
+                    })).handle
+                else if (pipe_details.out) b: {
+                    cmd.fd_set = try std.posix.pipe();
+                    res.items[i+1].opts.pipe_details.in = true;
+                    break :b cmd.fd_set[1];
+                } else
+                    term.stdout_file.handle;
+
             if (pipe_details.file.append)
-                try file.seekFromEnd(0);
-            const in_fd, const out_fd = switch (pipe_details.file.in_or_out) {
-                .IN => .{ file.handle, term.stdout_file.handle },
-                .OUT => .{ term.stdin_file.handle, file.handle },
-            };
+                try (std.fs.File{ .handle = out_fd }).seekFromEnd(0);
             cmd.fd_set = [2]std.posix.fd_t{ in_fd, out_fd };
         } else if (pipe_details.out) if (res.items.len > i+1) {
             cmd.fd_set = try std.posix.pipe();
@@ -182,21 +197,25 @@ pub fn parse_and_run(
         ) orelse {
             cmd.pid = try std.posix.fork();
             if (cmd.pid == 0) {
+                errdefer std.posix.abort;
+                defer std.posix.abort();
 
                 //stdin
-                try std.posix.dup2(
+                std.posix.dup2(
                     if (cmd.opts.pipe_details.in and !@constCast(cmd).wants_file_direction(.IN))
                         (&res.items[i - 1]).fd_set[0]
                     else
                         cmd.fd_set[0],
                     std.posix.STDIN_FILENO
-                );
+                ) catch |e|
+                    @panic(@errorName(e));
 
                 //stdout
-                try std.posix.dup2(
+                std.posix.dup2(
                     cmd.fd_set[1],
                     std.posix.STDOUT_FILENO
-                );
+                ) catch |e|
+                    @panic(@errorName(e));
 
                 for (res.items) |com| if (com.opts.pipe_details.out) {
                     std.posix.close(com.fd_set[0]);
@@ -216,6 +235,14 @@ pub fn parse_and_run(
                     "failed to run command: {?s} ({t})",
                     .{ cmd.split[0], final.err orelse unreachable }
                 );
+
+                if (cmd.opts.pipe_details.in) for (res.items[0..i]) |*proc|
+                    std.posix.kill(proc.pid, 9) catch |e|
+                        term.print_error(
+                            "failed stop chain: {?s} ({t})",
+                            .{ cmd.split[0], e }
+                        );
+
                 std.posix.exit(1);
             }
             if (!cmd.opts.piped and cmd.opts.wait) {
@@ -236,10 +263,10 @@ pub fn parse_and_run(
     }
 
     //close file descriptors (they're duped in forked processes)
-    for (res.items) |*cmd| if (cmd.opts.pipe_details.out) {
-        std.posix.close(cmd.fd_set[0]);
-        std.posix.close(cmd.fd_set[1]);
-    };
+     for (res.items) |*cmd| if (cmd.opts.pipe_details.out) {
+         std.posix.close(cmd.fd_set[0]);
+         std.posix.close(cmd.fd_set[1]);
+     };
 
     //wait for each command to finish
     for (res.items) |cmd| if (!cmd.is_builtin and cmd.opts.wait) {
