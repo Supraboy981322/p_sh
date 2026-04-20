d647a00f3178a7863d1e17bf02988e3ebc4f4640
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-20 14:12:02 -0500

    better state file iterator

--- src/state.zig
+++ src/state.zig
@@ -1,36 +1,15 @@
 const std = @import("std");
 const Term = @import("term.zig").Term;
 
-pub const Itr = struct {
-    reader:*std.Io.Reader,
-
-    pub fn init(file:std.fs.File) !Itr {
-        var buf:[1024]u8 = undefined;
-        var reader = file.reader(&buf).interface;
-        return .{
-            .reader = &reader,
-        };
-    }
-
-    pub fn next(self:*Itr) !?struct { name:[]const u8, value:[]const u8 } {
-        const line =
-            self.reader.takeDelimiterExclusive('\n') catch |e|
-                return if (e != error.EndOfStream)
-                    e
-                else
-                    null;
-        var split = std.mem.splitAny(u8, line, "=");
-        return .{
-            .name = split.first(),
-            .value = split.next() orelse return error.MissingValue,
-        };
-    }
+pub const Names = enum {
+    PWD,
 };
 
 pub fn init(term:*Term) !void {
     const home = term.env.get("HOME") orelse return;
     const path = try std.fs.path.join(term.alloc, &.{ home, ".cache", "p_sh_state" });
     defer term.alloc.free(path);
+
     var file = std.fs.openFileAbsolute(path, .{ .mode = .read_write }) catch |e| b: {
         if (e == error.FileNotFound) {
             break :b try std.fs.createFileAbsolute(path, .{ .read = true });
@@ -40,15 +19,42 @@ pub fn init(term:*Term) !void {
     defer file.close();
 
     if ((try file.stat()).size == 0) for ([_][]const u8{
-        "SHLVL=", @constCast(try term.get_env_orerr("SHLVL")), "\n",
         "PWD=", @constCast(try term.get_env_orerr("PWD")), "\n",
     }) |chunk| {
         _ = try file.write(chunk);
     };
-    var itr:Itr = try .init(file);
-    while (try itr.next()) |pair| {
-        const name = pair.name;
-        const value = pair.value;
-        std.debug.print("{s}={s}\n", .{name, value});
+
+    try file.seekTo(0);
+    var reader_buf:[std.fs.max_path_bytes + 1024]u8 = undefined;
+    var reader = file.reader(&reader_buf);
+    var buf = try std.ArrayList(u8).initCapacity(term.alloc, 0);
+    defer buf.deinit(term.alloc);
+    var name:Names = undefined;
+    while (
+        reader.interface.takeByte() catch |e|
+            if (e != error.EndOfStream)
+                return e
+            else
+                null
+    ) |b| {
+        switch (b) {
+            '\n' => if (buf.items.len > 0) {
+                std.debug.print("{s}={s}\n", .{@tagName(name), buf.items});
+                switch (name) {
+                    .PWD => {
+                        try term.env.put("OLDPWD", buf.items);
+                    }
+                }
+                buf.clearAndFree(term.alloc);
+            },
+            '=' => {
+                name = std.meta.stringToEnum(Names, buf.items) orelse {
+                    //_ = try reader.interface.discardDelimiterInclusive('\n');
+                    continue;
+                };
+                buf.clearAndFree(term.alloc);
+            },
+            else => try buf.append(term.alloc, b),
+        }
     }
 }
