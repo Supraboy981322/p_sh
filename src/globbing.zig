39068832834cd40c11f68a4afb60088701d8f863
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-04-22 13:32:27 -0500

    helper for file globbing

---
+++ src/globbing.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const glob = @import("glob");
+const Term = @import("term.zig").Term;
+
+pub fn match(term:*Term, pattern:[]u8) ![][]u8 {
+    var cwd = try term.cwd();
+
+    var res = try std.ArrayList([]u8).initCapacity(term.alloc, 1);
+    defer res.deinit(term.alloc);
+
+    var itr = cwd.iterate();
+    while (try itr.next()) |entry| if (glob.match(pattern, entry.name)) {
+        try res.append(term.alloc, try term.alloc.dupe(u8, entry.name));
+    };
+
+    if (res.items.len < 1)
+        return error.NoMatches;
+
+    return try res.toOwnedSlice(term.alloc);
+}
